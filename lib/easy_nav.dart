d909708d0140266b4226c90c3f349b5f2feed055
abdushakoor12 <abdushakoor009@yahoo.com>
2022-01-17 12:39:36 +0500

    Added a way to provide NavManager and DialogManager through EasyNav class

--- lib/easy_nav.dart
+++ lib/easy_nav.dart
@@ -1,5 +1,7 @@
 library easy_nav;
 
+import 'package:easy_nav/src/dialog_manager.dart';
+import 'package:easy_nav/src/nav_manager.dart';
 import 'package:flutter/material.dart';
 
 export 'src/dialog_manager.dart';
@@ -8,4 +10,15 @@ export 'src/nav_manager.dart';
 class EasyNav {
   static final GlobalKey<NavigatorState> navigatorKey =
       GlobalKey<NavigatorState>();
+
+  static NavManager? _navManager;
+  static DialogManager? _dialogManager;
+
+  static NavManager get navManager {
+    return _navManager ??= NavManager();
+  }
+
+  static DialogManager get dialogManager{
+    return _dialogManager ??= DialogManager();
+  }
 }
